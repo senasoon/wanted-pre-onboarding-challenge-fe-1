a15a0e4fe85ef6b45eb73389cbad5500cb780d52
senasoon <senasoon0@gmail.com>
2023-01-05 22:40:04 +0900

    Design: 회원가입 마크업

---
+++ src/components/signup/SignUp.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <form>
+      <fieldset>
+        <legend>회원가입</legend>
+        <label for="id">아이디</label>
+        <input name="id" id="id" />
+        <label for="password">비밀번호</label>
+        <input name="password" id="password" />
+        <button>회원가입</button>
+      </fieldset>
+    </form>
+  );
+};
+
+export default SignUp;
