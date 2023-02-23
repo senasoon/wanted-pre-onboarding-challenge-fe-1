cfb94a0399e0c2dd0b5da43e928ae379ca4a10e2
senasoon <senasoon0@gmail.com>
2023-02-23 20:53:59 +0900

    Rename: 파일 이름 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { LoginPage, MainPage, SignUpPage, TodoDetailPage } from './pages';
+import { LoginPage, MainPage, SignUpPage, TodoDetailPage } from 'pages';
 
 const App = () => {
   return (
     <Routes>
-      <Route exact path="/" element={<MainPage />} />
+      <Route path="/" element={<MainPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/detail/:id" element={<TodoDetailPage />} />
